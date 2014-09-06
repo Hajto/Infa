4df2e526a6defac1f468fad7bcfa40477d0c4332
Hajtosek <hajtosek@gmail.com>
2014-09-06 12:37:54 +0200

    Update

--- Wymienniki ciepła/Dystrybucja ciepła drugie podejście.cpp
+++ Wymienniki ciepła/Dystrybucja ciepła drugie podejście.cpp
@@ -16,14 +16,15 @@ int main(){
             pojemnik_1[i]=100;
             pojemnik_2[i]=0;
         }
-
+    double temp;
     //Mieszanie według fotki tablicy prog. Pawła K.
     for(int i = n; i >= 0; i--){
         for(int j = 0; j < n; j++){
-            double temp = srednia(pojemnik_1[j], pojemnik_2[i]);
+            temp = srednia(pojemnik_1[i], pojemnik_2[j]);
+           // cout<<"\n\t"<<pojemnik_1[i]<<"\t"<<pojemnik_2[j]<<"\t"<<temp<<"\n";
             pojemnik_1[i]=temp;
             pojemnik_2[j]=temp;
-            cout<<"\t"<<temp<<"\t";
+           // cout<<"\t"<<temp<<"\t";
         }
     }
     cout<<"\n";
@@ -34,21 +35,23 @@ int main(){
     double suma=0,sredniaztablicy;
     for(int i = 0; i < n; i++){
         suma += pojemnik_1[i];
-        cout<<"\t"<<pojemnik_1[i];
+        cout<<pojemnik_1[i]<<"\t";
     }
     sredniaztablicy = suma/n;
-    cout<<"\n\n\n"<<sredniaztablicy<<"\n\n\n";
+    printf("\n\nSrednia temperatur wody w pojemnikach, ktore na poczatku mialy goraca wode wynosi: %.3f \n\n", sredniaztablicy);
 
     //Średnia z drugiej tablicy
 suma=0;
     for(int i = 0; i < n; i++){
         suma += pojemnik_2[i];
-        cout<<"\t"<<pojemnik_2[i];
+        cout<<pojemnik_2[i]<<"\t";
     }
     sredniaztablicy = suma/n;
-    cout<<"\n\n\n"<<sredniaztablicy<<"\n\n\n";
+    printf("\n\nSrednia temperatur wody w pojemnikach, ktore na poczatku mialy zimna wode wynosi: %.3f \n", sredniaztablicy);
 
-
+    while ('\n' != getchar ());
+    printf ("Wcisnij enter aby zakonczyc prace programu.");
+    getchar ();
     return 0;
 }
 
