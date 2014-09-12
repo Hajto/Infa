3bdc35b79b936ad222a068a613b7dc5ee8437622
Hajtosek <hajtosek@gmail.com>
2014-09-12 20:21:56 +0200

    Summarka, update ignore

--- Wymienniki ciepła/Zapałki.cpp
+++ Wymienniki ciepła/Zapałki.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     int a;
         cin>>a;
-            a= a*2 + a/3 + 3;
+           if(a > 0)(a!= 1 ? a= a*2 + a/3 + 3 : a = 4);
         cout<<a;
 	return 0;
 }
