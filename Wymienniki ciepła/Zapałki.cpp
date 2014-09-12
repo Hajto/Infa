9ca65c1a4825651a0455707690f33aeba0562e3b
Hajtosek <hajtosek@gmail.com>
2014-09-12 20:15:49 +0200

    Zapałki

---
+++ Wymienniki ciepła/Zapałki.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int a;
+        cin>>a;
+            a= a*2 + a/3 + 3;
+        cout<<a;
+	return 0;
+}
